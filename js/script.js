d4a76b4c716f861a42c1f927b3be6a662f304a63
Semvel <43538564+Semvel@users.noreply.github.com>
2020-08-13 13:03:05 +0500

    доверстал 5-6 блок

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// подмена видео
+// подмена видео1
 function findVideos() {
     let videos = document.querySelectorAll('.video');
     
@@ -43,6 +43,55 @@ function generateURL(id) {
 }
 
 findVideos();
+
+// подмена видео-2
+
+function findVideos2() {
+    let videos2 = document.querySelectorAll('.video-2');
+    
+    for (let i = 0; i < videos2.length; i++) {
+        setupVideo2(videos2[i]);
+    }        
+}
+
+function setupVideo2(video2) {
+    let link2 = video2.querySelector('.video-2__link');
+    let media2 = video2.querySelector('.video-2__media');
+    let button2 = video2.querySelector('.video-2__button');
+    let id2 = 'ZbB5mPO5yPo';
+
+    video2.addEventListener('click', () => {
+        let iframe2 = createIframe2(id2);
+
+        link2.remove();
+        button2.remove();
+        video2.appendChild(iframe2);
+    });
+    
+    link2.removeAttribute('href');
+    video2.classList.add('video--enabled');
+}
+
+function createIframe2(id2) {
+    let iframe2 = document.createElement('iframe');
+
+    iframe2.setAttribute('allowfullscreen', '');
+    iframe2.setAttribute('allow', 'autoplay');
+    iframe2.setAttribute('src', generateURL2(id2));
+    iframe2.classList.add('video-2__media');
+    iframe2.setAttribute('width', '100%');
+    iframe2.setAttribute('hight', '100%');
+    return iframe2;
+}
+
+function generateURL2(id2) {
+    let query2 = '?rel=0&showinfo=0&autoplay=1&controls=0';
+
+    return 'https://www.youtube.com/embed/' + id2 + query2;
+}
+
+findVideos2();
+
 
 //скрипт слайдера
 
